147be53f05d6bc0bced2984b20778f6f923a01df
Sujan Gyawali <gyasujan@gmail.com>
2024-09-01 21:26:23 +0545

    Continue: work on logging the file

--- lib/logger.py
+++ lib/logger.py
@@ -1,9 +1,22 @@
 import datetime
 import os
+from config.constants import *
 
 class Logger:
     def __init__(self, script_name):
-        current_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z")
+        current_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%")
         log_path = os.getenv("LOG_PATH")
-        log_file_name = str(script_name) + "_" +str(current_time) + ".log"
+        log_file_name = str(script_name) + "_" + str(current_time) + ".log"
         log_file = os.path.join(log_path, log_file_name)
+        self.log_file = open(log_file,'w')
+
+    def log_message(self, msg, level = "INFO", boxed = True):
+        now = datetime.datetime.now()
+        log_level = level.upper()
+        log_msg = f"[{now}]: {log_level} - {msg}\n"
+        log_msg += "\n******************************************************************************************"
+        self.log_file.write(log_msg)
+        self.log_file.flush()
+        
+    def close(self):
+        self.log_file.close()
--- src/extraction/category_extraction.py
+++ src/extraction/category_extraction.py
@@ -1,5 +1,10 @@
 import os
 from dotenv import load_dotenv
+from lib.logger import Logger
+
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
+log = Logger("category_extraction")
+log.log_message("Scripts for category has been started")
+log.close
