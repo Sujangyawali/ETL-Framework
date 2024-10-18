afac006a64941c2d251abfe700cde1334e16de44
Sujan Gyawali <gyasujan@gmail.com>
2024-10-18 20:32:35 +0545

    Update on database extractor lib:Added feature to handling and aduting data

--- lib/mysql_database_extractor.py
+++ lib/mysql_database_extractor.py
@@ -50,39 +50,34 @@ class MySQLDataExtractor:
             raise Exception(f"Error executing query: {e}")
     
     def extract_to_csv(self, query: str, csv_file_name:str, chunk_size: int = 1000):
-        self.log.log_message(f"Starting extraction to CSV from query: {query}")
-        try:
-            self.cursor.execute(f"USE {self.database}")
-            self.log.log_message(f"Query using database: {self.database}")
-            csv_file_name = os.oath.join(DATA_DIR,f"{csv_file_name}_{datetime.now().strftime("%Y%m%d%H%M%S")}.csv")
-            self.log.log_message(f"Writing data to CSV file: {csv_file_name}")
-            with open(csv_file_name, mode='w', newline='', encoding='utf-8') as file:
-                header = [col[0] for col in self.cursor.description]
-                file.write(','.join(header) + '\n')
-                total_rows = 0 
-                while True:
-                    data_chunk = self.cursor.fetchmany(chunk_size)
-                    if not data_chunk:
-                        break
-                    
-                    for row in data_chunk:
-                        file.write(','.join(str(row[col]) for col in header) + '\n')
-                    total_rows += len(data_chunk)
-                    self.log.log_message(f"Data extraction to CSV completed. Total rows written: {total_rows}")
-        except Exception as e:
-            self.log.log_message(f"Error during extraction to CSV: {e}")
-            raise Exception(f"Error during extraction to CSV: {e}")
+            try:
+                self.cursor.execute(f"USE {self.database}")
+                self.cursor.execute(query)
+                total_rows_fetched = self.cursor.rowcount
+                self.log.log_message(f"Query executed successfully. Total rows fetched: {total_rows_fetched}")
+            except Exception as qe:
+                qe = qe.args[1] if len(qe.args) > 1 else str(qe)
+                self.log.log_message(f"Error executing query: {qe}")
+                raise Exception(f"Error executing query: {qe}")
+            csv_file_name = os.path.join(DATA_DIR,f"{csv_file_name}_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv")
+            
+            if total_rows_fetched != 0:
+                self.log.log_message(f"Writing data to CSV file: {csv_file_name}")
+                try:
+                    with open(csv_file_name, mode='w', newline='', encoding='utf-8') as file:
+                        header = [col[0] for col in self.cursor.description]
+                        file.write(','.join(header) + '\n')
+                        while True:
+                            data_chunk = self.cursor.fetchmany(chunk_size)
 
-
-
-
-
-
-
-
-
-
-
+                            if not data_chunk:
+                                break
+                            
+                            for row in data_chunk:
+                                file.write(','.join(str(row[col]) for col in header) + '\n')
+                        self.log.log_message(f"Data extraction to CSV completed.")
+                except Exception as csve:
+                    self.log.log_message(f"Error writing data to CSV file: {csve}")
     def end_connection(self):
         self.connection.close()
         self.log.log_message("Database Session Closed")
