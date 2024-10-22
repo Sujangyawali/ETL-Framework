a309148097db3dfcf67d3a2ffbb277037aba7fe0
Sujan Gyawali <gyasujan@gmail.com>
2024-10-22 23:22:34 +0545

    update get_file method to get file from the provided directory

--- lib/SFTP_extractor.py
+++ lib/SFTP_extractor.py
@@ -21,11 +21,25 @@ class SFTP:
                 self.sftp = pysftp.Connection(host = self.host, username = self.username, password = self.password, cnopts= cnopts)
             self.log.log_message("Connection to SFTP server established")
         except Exception as ce:
-            self.log.log_message("Error connecting to SFTP server: " + str(ce))
+            self.log.log_message(f"Error connecting to SFTP server: " + str(ce))
             raise Exception(f"Error connecting to SFTP server:" + str(ce))
-    
-    def get_file(self):
-        pass
+        
+
+    def get_file(self, dir): #extracts one file per directory
+        try:
+            self.log.log_message(f"Checking file in the directory:{dir}")
+            files = self.sftp.listdir(dir)
+            # incase of multiple files pick latest one
+            if len(files)>1:
+                latest_file = sorted(files, key=lambda x: x.rsplit('_', 1)[-1])[-1]
+                return latest_file
+            if len(files) == 1:
+                return files[0]
+            else:
+                return None
+        except Exception as fe:
+            self.log.log_message(f"Error getting file from SFTP server: " + str(fe))
+            raise Exception(f"Error getting file from SFTP server: " + str(fe))
 
     def put_file(self):
         pass
