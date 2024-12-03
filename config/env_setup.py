49e799e8fcdf21eaab67044edc76ce6181811685
Sujan Gyawali <gyasujan@gmail.com>
2024-12-03 20:45:58 +0545

    Deletion on unused variable

--- config/env_setup.py
+++ config/env_setup.py
@@ -8,7 +8,6 @@ API_KEY = os.getenv('API_KEY')
 API_HOST = os.getenv('API_HOST')
 LOG_PATH = os.getenv('LOG_PATH')
 ROOT_URL = os.getenv('ROOT_URL')
-ROOT_URL = os.getenv('DATA_DIR')
 
 #MYSQL DATABSE ENV VARIABLES
 DB_HOST = os.getenv('DB_HOST')
